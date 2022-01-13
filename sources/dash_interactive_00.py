06a68fb4b4363e9b6193362bc966007236382256
er-ssmohanty <pie2001@protonmail.com>
2022-01-13 23:30:01 +0530

    minor changes

--- sources/dash_interactive_00.py
+++ sources/dash_interactive_00.py
@@ -5,6 +5,6 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 
-airline_data =  pd.read_csv('~/py/data/airline_data.csv', 
+airline_data =  pd.read_csv('../data/airline_data.csv', 
                                                       encoding = "ISO-8859-1",dtype={'Div1Airport': str, 'Div1TailNum': str, 
                                    'Div2Airport': str, 'Div2TailNum': str})
