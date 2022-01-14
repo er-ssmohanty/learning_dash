46a0400d6c3898f00e7b79a97b0a97f21bc6d459
er-ssmohanty <pie2001@protonmail.com>
2022-01-14 20:32:15 +0530

    finally the graded assignmen t started

--- scripts/Dash_Auto.py
+++ scripts/Dash_Auto.py
@@ -14,7 +14,7 @@ from dash import no_update
 #
 
 app  = dash.Dash(__name__)
-#app.config.suppress_callback_exceptions = True
+app.config.suppress_callback_exceptions = False #True
 auto_data = pd.read_csv('../data/automobileEDA.csv',encoding="ISO-8859-1")
 
 app.layout = html.Div(children=[#TASK 3A
@@ -22,46 +22,41 @@ app.layout = html.Div(children=[#TASK 3A
                                 style={'textAlign': 'center', 'color': '#503D36',
                                 'font-size': 24}),
      html.Div([
-                    html.Div(
-                            html.H2('Drive Wheels Type:', style={'margin-right': '2em'}),
-                     ),
-                    dcc.Dropdown(id='demo-dropdown',options=[
-                        {'label': 'Rear Wheel Drive', 'value': 'rwd'},
-                        {'label': 'Front Wheel Drive', 'value': 'fwd'},
-                        {'label': 'Four Wheel Drive', 'value': '4wd'}],
-                        value='rwd'
-                    ),
-
-                    #Second Inner division for adding 2 inner divisions for 2 output graphs
-                    html.Div([
-
-                        #TASK 3D
-                        html.Div([ ], id='plot1'),
-                        html.Div([ ], id='plot2')
-
-                    ], style={'display': 'flex'}),
-
-
+        html.Div(
+            html.H2('Drive Wheels Type:', style={'margin-right': '2em'}),
+        ),
+        #
+        dcc.Dropdown(id='demo-dropdown',options=[
+            {'label': 'Rear Wheel Drive', 'value': 'rwd'},
+            {'label': 'Front Wheel Drive', 'value': 'fwd'},
+            {'label': 'Four Wheel Drive', 'value': '4wd'}],
+            value='rwd'
+        ),
+        #
+        html.Div([
+            html.Div([ ], id='plot1'),
+            html.Div([ ], id='plot2')
+        ],
+        #
+        style={'display': 'flex'}),
     ])
-    #outer division ends
-
 ])
-#layout ends
 
-#Place to add @app.callback Decorator
-#TASK 3E
+#
+#
+#
+#
+#
+
 @app.callback([Output(component_id='plot1', component_property='children'),
                Output(component_id='plot2', component_property='children')],
                Input(component_id='demo-dropdown', component_property='value'))
 
-#Place to define the callback function .
-#TASK 3F
 def display_selected_drive_charts(value):
    filtered_df = auto_data[auto_data['drive-wheels']==value].groupby(['drive-wheels','body-style'],as_index=False).mean()
    fig1 = px.pie(filtered_df, values='price', names='body-style', title="Pie Chart")
    fig2 = px.bar(filtered_df, x='body-style', y='price', title='Bar Chart')
-   return [dcc.Graph(figure=fig1),
-            dcc.Graph(figure=fig2) ]
+   return [dcc.Graph(figure=fig1),dcc.Graph(figure=fig2)]
 #
 #
 #
